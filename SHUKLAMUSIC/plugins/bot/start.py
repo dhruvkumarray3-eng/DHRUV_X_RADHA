8dc1c5bc02ef7fc429d4b5d03f50812c64291a06
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2026-05-28 15:31:06 +0530

    Update start.py

--- SHUKLAMUSIC/plugins/bot/start.py
+++ SHUKLAMUSIC/plugins/bot/start.py
@@ -128,7 +128,6 @@ async def start_gp(client, message: Message, _):
         random.choice(SHASHANK_IMG),
         caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
         reply_markup=InlineKeyboardMarkup(out),
-        message_effect_id=random.choice(EFFECT_IDS),
     )
     return await add_served_chat(message.chat.id)
 
